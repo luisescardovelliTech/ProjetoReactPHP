994a4fbb0a455debbc202f780c8ff7bc0800e25c
Luís Felipe Escardovelli Silva <escardovellilipe@outlook.com>
2025-05-19 12:35:47 -0300

    adicionando rotas e o crud na tela

--- front-end/financias/src/App.js
+++ front-end/financias/src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
+import Crud from './Crud';
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-      
-      </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<div>Bem-vindo ao Organiza Grana!</div>} />
+        <Route path="/crud" element={<Crud />} />
+        {/* Outras rotas aqui */}
+      </Routes>
+    </Router>
   );
 }
 
--- front-end/financias/src/Navbar.js
+++ front-end/financias/src/Navbar.js
@@ -1,28 +1,29 @@
-import React, {useState} from "react";
+import { Link } from 'react-router-dom';
+import React, { useState } from "react";
 import './Navbar.css';
 
 function Navbar (){
     const [aberto, setAberto] = useState(false);
 
-function abrirFechar(){
-    setAberto(!aberto);
-} 
+    function abrirFechar(){
+        setAberto(!aberto);
+    } 
 
-return(
-    <nav className="navbar">
-      <div className="navbar-logo">Organiza Grana</div>
-      <button className="navbar-toggle" onClick={abrirFechar}>
-        ☰
-      </button>
-      <ul className={`navbar-links ${aberto ? "active" : ""}`}>
-        <li><a href="#home">Início</a></li>
-        <li><a href="#crud">Organizar Grana</a></li>
-        <li><a href="#sobre">Sobre</a></li>
-        <li><a href="#servicos">Serviços</a></li>
-        <li><a href="#contato">Contato</a></li>
-      </ul>
-    </nav>
-);
+    return(
+        <nav className="navbar">
+            <div className="navbar-logo">Organiza Grana</div>
+            <button className="navbar-toggle" onClick={abrirFechar}>
+                ☰
+            </button>
+            <ul className={`navbar-links ${aberto ? "active" : ""}`}>
+                <li><Link to="/">Início</Link></li>
+                <li><Link to="/crud">Organizar Grana</Link></li>
+                <li><a href="#sobre">Sobre</a></li>
+                <li><a href="#servicos">Serviços</a></li>
+                <li><a href="#contato">Contato</a></li>
+            </ul>
+        </nav>
+    );
 }
 
 export default Navbar;
